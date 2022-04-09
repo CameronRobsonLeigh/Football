a8312c38c8461549e48564124eebe0a380f5e919
famus99 <73717006+famus99@users.noreply.github.com>
2022-04-09 20:42:22 +0100

    added new pages

--- home.py
+++ home.py
@@ -2,6 +2,7 @@
 # username = Cameron789
 # env\Scripts\activate
 # set FLASK_APP=Home
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 
 from flask import request
 from flask import Flask
@@ -9,9 +10,11 @@ from flask import render_template
 import requests
 import mysql.connector
 from mysql.connector.constants import ClientFlag
+from flask import session
 
 # initializes the app (__name__ = the file name)
 app = Flask(__name__)
+app.secret_key = 'sessionkey'
 
 config = {
     'user': 'root',
@@ -28,9 +31,18 @@ def index():
 def login():
     return render_template("login.html")
 
+@app.route('/LoginNew')
+def loginNew():
+    return render_template("loginnew.html")
+
 @app.route('/Registration')
 def Registration():
     return render_template("registration.html")
+
+@app.route('/Settings')
+def UserSettings():
+    print(session['userid'])
+    return render_template("UserSettings.html")
 
 
 @app.route('/handle_data', methods=["POST"])
@@ -69,36 +81,32 @@ def displayTeams():
 
 @app.route('/register', methods=['POST'])
 def register():
-    # name = request.form['name']
-    # password = request.form['password']
-    # email = request.form['email']
-    # teamID = request.form['favouriteTeam']
-    cnxn = mysql.connector.connect(**config)
+    name = request.form['name']
+    password = request.form['password']
+    email = request.form['email']
+    teamID = request.form['favouriteTeam']
+    con = mysql.connector.connect(**config)
+    cursor = con.cursor()
+    
+    cursor.execute("INSERT INTO Users (name, password, email) VALUES (%s, %s, %s)", (name, password, email))
+    con.commit()  # and commit changes
 
-    mycursor = cnxn.cursor()
-    mycursor.execute("DESCRIBE Users")
-    for x in mycursor:
-        print(x)
-    # cursor.execute("INSERT INTO Users (name, password, email) VALUES (%s, %s, %s)", (name, password, email))
-    # con.commit()  # and commit changes
-
-    # cursor = con.cursor()
-    # cursor.execute("SELECT UserId FROM Users WHERE email = %s", ([email]))
-    # out = cursor.fetchall()
+    cursor.execute("SELECT UserId FROM Users WHERE email = %s", ([email]))
+    out = cursor.fetchall()
 
 
-    # grabTeams = ""
-    # for row in out:
-    #      cursor = con.cursor()
-    #      cursor.execute("INSERT INTO FavouriteTeam (userID, teamID) VALUES (%s, %s)", (row[0], teamID))
-    #      con.commit()  # and commit changes
+    grabTeams = ""
+    for row in out:
+         cursor = con.cursor()
+         cursor.execute("INSERT INTO FavouriteTeam (userID, teamID) VALUES (%s, %s)", (row[0], teamID))
+         con.commit()  # and commit changes
 
-    #      cursor = con.cursor()
-    #      cursor.execute("SELECT * FROM FavouriteTeam WHERE userId = %s", ([row[0]]))
-    #      grabTeams = cursor.fetchone()
+         cursor = con.cursor()
+         cursor.execute("SELECT * FROM FavouriteTeam WHERE userId = %s", ([row[0]]))
+         grabTeams = cursor.fetchone()
 
-    # id ,user, team = grabTeams
-    return "HELLO"
+    id ,user, team = grabTeams
+    return redirect(url_for('loginNew'))
 
 
 @app.route('/userlogin', methods=['POST'])
@@ -106,7 +114,6 @@ def userlogin():
     email = request.form['email']
     password = request.form['password']
 
-    config['database'] = 'footballdb'  # add new database to config dict
     con = mysql.connector.connect(**config)
 
     cursor = con.cursor()
@@ -114,6 +121,24 @@ def userlogin():
     out = cursor.fetchall()
 
     if len(out) == 0:
-        return render_template("login.html", errorMessage = "Invalid Credentials")
+        return render_template("loginnew.html", errorMessage = "Invalid Credentials")
     else:
-        return render_template("index.html")
+        cursor.execute("SELECT UserId FROM Users WHERE email = %s", ([email]))
+        out = cursor.fetchone()
+        userid = out
+
+        # cursor.execute("SELECT * FROM FavouriteTeam")
+        # out2 = cursor.fetchall()
+        # for x in out2:
+        #     print(x)
+        
+
+
+
+        # cursor = con.cursor()
+        # cursor.execute("SELECT teamID FROM FavouriteTeam WHERE userId = %s", (userid))
+        # grabTeam = cursor.fetchone()
+
+        session['userid'] = userid
+        # session['teamid'] = grabTeam
+        return redirect(url_for('UserSettings'))
